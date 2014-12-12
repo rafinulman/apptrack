11e081eb6e4ac1a925f2329ba4c46437749ebd71
rafinulman <rafinulman@gmail.com>
2014-12-12 10:28:32 -0600

    Small tweaks....
    finished adding validation

--- app/models/jobapp.rb
+++ app/models/jobapp.rb
@@ -3,5 +3,12 @@ class Jobapp < ActiveRecord::Base
 	belongs_to :company
 	has_many :todo
 	has_many :contact
+
+	def description
+		return self.position + " at " + self.company.name
+	end
+	validates :company_id, :presence => true
+	validates :position, :presence => true
+	validates :interview_on, :presence => true
 end
 
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,4 +2,7 @@ class Todo < ActiveRecord::Base
 	belongs_to :jobapp
 	belongs_to :action
 	belongs_to :contact
+
+	validates :due_at, :presence => true
+	validates :action_id, :presence => true
 end
